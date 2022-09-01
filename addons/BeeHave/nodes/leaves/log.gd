cc810804ca061909179c397fa2a587a76c2e58b1
dengxuan <you@example.com>
2022-09-01 17:24:10 +0800

    修改文件名大小写

--- addons/BeeHave/nodes/leaves/log.gd
+++
@@ -1,9 +0,0 @@
-class_name LogAction,  '../../icons/log.svg'
-extends Action
-
-export(String, MULTILINE) var text:String = 'hello'
-
-
-func tick(actor, blackboard:Blackboard) -> int:
-	print_debug(text)
-	return SUCCESS
